b9a1563b7964b0fbfe1356b13e2d5649e9e8eb59
BenGalewsky <ben@peartreestudio.net>
2021-03-13 19:57:52 -0600

    Add script to load active owners into dynamo db

---
+++ load_active_owners.py
@@ -0,0 +1,21 @@
+import csv
+import boto3
+"""
+This script reads a csv file of active owners and inserts them into the 
+Dynamo db. The primary key of this table is the owner ID
+"""
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cgfc_owners')
+with open('Active Owner Report as of 08.27.20.csv', mode='r', encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for owner in csv_reader:
+        print(owner['OwnerNum'], owner['LastName'], owner['FirstName'])
+        item = {
+            'owner_id': owner['OwnerNum'],
+            'first_name': owner['FirstName'],
+            'last_name': owner['LastName'],
+            'email': owner['Email']
+        }
+        table.put_item(Item=item)
+
